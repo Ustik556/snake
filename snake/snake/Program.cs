bc230de9d8535b41b32f82ee1e87480da3270c64
Oleg <ystikoo@mail.ru>
2016-10-03 19:56:07 +0300

    инко

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,16 +10,10 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-             p1.x = 1;
-             p1.y = 4;
-             p1.sim = '*';
+            Point p1 = new Point(1,4,'*');
              p1.Drow();
 
-            Point p2 = new Point();
-             p2.x = 2;
-             p2.y = 6;
-             p2.sim = '$';
+            Point p2 = new Point(2,6,'$');
              p2.Drow();
 
 
