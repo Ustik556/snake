71286fb486ee0d134604e3d66aa0348f1aa6ba7c
Oleg <ystikoo@mail.ru>
2016-10-03 17:34:33 +0300

    Вывод символов на экран по координатам

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            int x1 = 1;
+            int y1 = 4;
+            char sim1 = '*';
+            Drow(x1, y1, sim1);
+
+            int x2 = 2;
+            int y2 = 5;
+            char sim2 = '$';
+            Drow(x2, y2, sim2);
+
             Console.ReadLine();
+        }
+        static void Drow(int x, int y, char sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
         }
     }
 }
